481fd85bda9f2ea2abcb0b8adc9cac66e191d31c
Ricardo Branco <ricardo.branco@smartmatic.com>
2017-02-09 17:31:05 -0500

    Use a map instead of a slice to support sparse bitsets

--- bitset.go
+++ bitset.go
@@ -13,34 +13,27 @@ const (
 )
 
 type Bitset struct {
-	words []word
+	words map[int]word
 	count int // Number of elements inserted
 	max   int // Keep track of the maximum number ever inserted
 }
 
 // Returns a new Bitset. Set argument to the expected maximum number or -1.
 func New(max int) *Bitset {
-	words := 1
+	bs := new(Bitset)
+	bs.max = max
+	bs.words = make(map[int]word)
 	if max >= 0 {
-		words = max / bitsPerWord
-		if max%bitsPerWord != 0 {
-			words++
+		for i := max / bitsPerWord; i >= 0; i-- {
+			bs.words[i] = 0
 		}
 	}
-	bs := new(Bitset)
-	bs.words = make([]word, words)
-	bs.max = max
 	return bs
 }
 
 func (bs *Bitset) Add(i int) {
 	if i < 0 {
 		panic(fmt.Sprintf("Trying to Add a negative number: %d\n", i))
-	}
-	if i >= len(bs.words)*bitsPerWord {
-		var newSet = make([]word, len(bs.words)+i/bitsPerWord+1)
-		copy(newSet, bs.words)
-		bs.words = newSet
 	}
 	bs.words[i/bitsPerWord] |= 1 << uint(i%bitsPerWord)
 	bs.count++
